712f53ec7d108271f8e5a78e15b3120b59032f1d
MayBMore <dh6927@naver.com>
2018-09-21 17:26:56 +0900

    03 exercise new

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) {
 	char c;
 	
-	printf("input a number:");
+	printf("enter a character : ");
 	scanf("%c", &c);
 	
 	printf("The next character of %c (%i) is %c (%i)\n", c, c, c+1, c+1);
